60aebb2ee00e59a9f4257b74466d1932e62e111b
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2022-07-27 14:28:58 +0100

    SCC_690 | Added contactDetails to Connector

--- app/uk/gov/hmrc/scachangeofcircumstances/connectors/IfConnector.scala
+++ app/uk/gov/hmrc/scachangeofcircumstances/connectors/IfConnector.scala
@@ -27,33 +27,40 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class IfConnector @Inject()(http: HttpClient, appConfig: AppConfig)(implicit executionContext: ExecutionContext) extends Logging {
 
-  val fields: String =
+  private val designatoryDetailsFields: String =
     "details(marriageStatusType),nameList(name(nameSequenceNumber,nameType,titleType," +
       "requestedName,nameStartDate,nameEndDate,firstForename,secondForename,surname))," +
       "addressList(address(addressSequenceNumber,countryCode,addressType,addressStartDate," +
       "addressEndDate,addressLine1,addressLine2,addressLine3,addressLine4,addressLine5," +
       "addressPostcode))"
 
+  private val contactDetailsFields: String = "contactDetails(code,type,detail)"
+
   def getDesignatoryDetails(nino: String)(implicit hc: HeaderCarrier): Future[IfDesignatoryDetails] = {
-    http.GET[IfDesignatoryDetails](s"${appConfig.ifBaseUrl}/individuals/details/nino/$nino?fields=$fields").recoverWith {
-      case validationError: JsValidationException =>
-        logger.warn(s"Integration Framework JsValidationException encountered: $validationError")
-        Future.failed(new InternalServerException("Something went wrong."))
-      case Upstream5xxResponse(msg, code, _, _) =>
-        logger.warn(s"Integration Framework Upstream5xxResponse encountered: $code, $msg")
-        Future.failed(new InternalServerException("Something went wrong."))
-      case Upstream4xxResponse(msg, 404, _, _) if msg.contains("IDENTIFIER_NOT_FOUND") =>
-        logger.warn(s"Integration Framework returned NiNo not found")
-        Future.failed(new NotFoundException(msg))
-      case Upstream4xxResponse(msg, code, _, _) =>
-        logger.warn(s"Integration Framework Upstream4xxResponse encountered: $code, $msg")
-        Future.failed(new InternalServerException("Something went wrong."))
-      case e: Exception =>
-        logger.warn(s"Integration Framework Exception encountered: ${e.getMessage}")
-        Future.failed(new InternalServerException("Something went wrong."))
-    }
+    http.GET[IfDesignatoryDetails](s"${appConfig.ifBaseUrl}/individuals/details/nino/$nino?fields=$designatoryDetailsFields")
+      .recoverWith(errorHandling[IfDesignatoryDetails])
   }
 
-  def getContactDetails(): Future[IfContactDetails] = ???
+  def getContactDetails(nino: String)(implicit hc: HeaderCarrier): Future[IfContactDetails] = {
+    http.GET[IfContactDetails](s"${appConfig.ifBaseUrl}/individuals/details/contact/nino/${nino}?fields=$contactDetailsFields")
+      .recoverWith(errorHandling[IfContactDetails])
+  }
 
+  private def errorHandling[T]: PartialFunction[Throwable, Future[T]] = {
+    case validationError: JsValidationException =>
+      logger.warn(s"Integration Framework JsValidationException encountered: $validationError")
+      Future.failed(new InternalServerException("Something went wrong."))
+    case Upstream5xxResponse(msg, code, _, _) =>
+      logger.warn(s"Integration Framework Upstream5xxResponse encountered: $code, $msg")
+      Future.failed(new InternalServerException("Something went wrong."))
+    case Upstream4xxResponse(msg, 404, _, _) if msg.contains("IDENTIFIER_NOT_FOUND") || msg.contains("PERSON_NOT_FOUND") =>
+      logger.warn(s"Integration Framework returned NiNo not found")
+      Future.failed(new NotFoundException(msg))
+    case Upstream4xxResponse(msg, code, _, _) =>
+      logger.warn(s"Integration Framework Upstream4xxResponse encountered: $code, $msg")
+      Future.failed(new InternalServerException("Something went wrong."))
+    case e: Exception =>
+      logger.warn(s"Integration Framework Exception encountered: ${e.getMessage}")
+      Future.failed(new InternalServerException("Something went wrong."))
+  }
 }
